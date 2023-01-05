5b2227ae26aaac5309ffdebd4f332525690d35dd
MONA DAVARI s296589 <s296589@studenti.polito.it>
2023-01-06 00:40:50 +0100

    add data and nazanines changes

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -1,18 +1,147 @@
+import torch
+from models.base_model import DomainDisentangleModel
+import numpy as np
 
-#TODO
+
+def myReconstructorLoss(reconstructorOutputs, features):
+    return torch.nn.MSELoss( reconstructorOutputs, features ) + torch.nn.KLDivLoss( reconstructorOutputs, features )
+
+
+def myEntropyLoss(outputs):
+    l = 0
+    for i in range( len( outputs ) ):
+        l += np.log( outputs[i] )
+    l *= 1 / len( outputs )
+    return -l
+
+
 class DomainDisentangleExperiment: # See point 2. of the project
     
     def __init__(self, opt):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        self.device = torch.device( 'cpu' if opt['cpu'] else 'cuda:0' )
 
-    def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        # Setup model
+        self.model = DomainDisentangleModel()
+        self.model.train()
+        self.model.to( self.device )
+        for param in self.model.parameters():
+            param.requires_grad = True
 
-    def load_checkpoint(self, path):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+            # Setup optimization procedure
+            self.object_classifier_optimizer = torch.optim.Adam( self.model.parameters(), lr=opt['lr'] )
+            self.object_classifier_criterion = torch.nn.CrossEntropyLoss()
 
-    def train_iteration(self, data):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+            self.domain_classifier_optimizer = torch.optim.Adam( self.model.parameters(), lr=opt['lr'] )
+            self.domain_classifier_criterion = torch.nn.CrossEntropyLoss()
 
-    def validate(self, loader):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+            self.domain_category_optimizer = torch.optim.Adam( self.model.parameters(), lr=opt['lr'] )
+            self.domain_category_criterion = myEntropyLoss()
+
+            self.object_domain_optimizer = torch.optim.Adam( self.model.parameters(), lr=opt['lr'] )
+            self.object_domain_criterion = myEntropyLoss()
+
+            self.reconstructor_optimizer = torch.optim.Adam( self.model.parameters(), lr=opt['lr'] )
+            self.reconstructor_criterion = myReconstructorLoss()
+
+        def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
+            checkpoint = {}
+
+            checkpoint['iteration'] = iteration
+            checkpoint['best_accuracy'] = best_accuracy
+            checkpoint['total_train_loss'] = total_train_loss
+
+            checkpoint['model'] = self.model.state_dict()
+            checkpoint['optimizer'] = self.optimizer.state_dict()
+
+            torch.save( checkpoint, path )
+            raise NotImplementedError( '[TODO] Implement DomainDisentangleExperiment.' )
+
+        def load_checkpoint(self, path):
+            checkpoint = torch.load( path )
+
+            iteration = checkpoint['iteration']
+            best_accuracy = checkpoint['best_accuracy']
+            total_train_loss = checkpoint['total_train_loss']
+
+            self.model.load_state_dict( checkpoint['model'] )
+            self.optimizer.load_state_dict( checkpoint['optimizer'] )
+
+            return iteration, best_accuracy, total_train_loss
+            raise NotImplementedError( '[TODO] Implement DomainDisentangleExperiment.' )
+
+        def train_iteration(self, data, train=True):
+            if train:
+                x, y = data
+                x = x.to( self.device )
+                y = y.to( self.device )
+
+                self.object_classifier_optimizer.zero_grad()
+                logits = self.model( x, w1=1 )
+                loss = self.object_classifier_criterion( logits, y )
+                loss.backward()
+                self.object_classifier_optimizer.step()
+
+                self.domain_classifier_optimizer.zero_grad()
+                logits = self.model( x, w2=1 )
+                loss = self.domain_classifier_criterion( logits )
+                loss.backward()
+
+                self.domain_category_optimizer.zero_grad()
+                logits = self.model( x, w3=1 )
+                loss = self.domain_category_criterion( logits )
+                loss.backward()
+
+                self.object_domain_optimizer.zero_grad()
+                logits = self.model( x, w4=1 )
+                loss = self.object_domain_criterion( logits )
+                loss.backward()
+                self.object_domain_optimizer.step()
+
+                self.reconstructor_optimizer.zero_grad()
+                logits = self.model( x, w5=1 )
+                loss = self.reconstructor_criterion( logits, x )
+                loss.backward()
+
+            else:
+                x = data
+                x = x.to( self.device )
+
+                logits = self.model( x, w2=1 )
+                loss = self.domain_classifier_criterion( logits )
+                loss.backward()
+                self.domain_category_optimizer.step()
+
+                logits = self.model( x, w3=1 )
+                loss = self.domain_category_criterion( logits )
+                loss.backward()
+                self.domain_category_optimizer.step()
+
+                logits = self.model( x, w5=1 )
+                loss = self.reconstructor_criterion( logits, x )
+                loss.backward()
+                self.reconstructor_optimizer.step()
+
+            return loss.item()
+
+        def validate(self, loader):
+            self.model.eval()
+            accuracy = 0
+            count = 0
+            loss = 0
+            with torch.no_grad():
+                for x, y in loader:
+                    x = x.to( self.device )
+                    y = y.to( self.device )
+
+                    logits = self.model( x )
+                    loss += self.criterion( logits, y )
+                    pred = torch.argmax( logits, dim=-1 )
+
+                    accuracy += (pred == y).sum().item()
+                    count += x.size( 0 )
+
+            mean_accuracy = accuracy / count
+            mean_loss = loss / count
+            self.model.train()
+            return mean_accuracy, mean_loss
+            raise NotImplementedError( '[TODO] Implement DomainDisentangleExperiment.' )
--- models/base_model.py
+++ models/base_model.py
@@ -17,12 +17,10 @@ class FeatureExtractor( nn.Module ):
         x = self.resnet18.layer3( x )
         x = self.resnet18.layer4( x )
         x = self.resnet18.avgpool( x )
-        temp = x.squeeze()
-
-        # if len( temp.size() ) < 2:
-        #     return x
-
-        return temp
+        x.squeeze()
+        if len( x.size() ) < 2:
+            return x.unsqueeze( 0 )
+        return x
 
 
 class BaselineModel( nn.Module ):
@@ -58,15 +56,56 @@ class DomainDisentangleModel( nn.Module ):
         super( DomainDisentangleModel, self ).__init__()
         self.feature_extractor = FeatureExtractor()
 
-        self.domain_encoder = None  # TODO
-        self.category_encoder = None  # TODO
+        self.domain_encoder = nn.Sequential(
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU(),
 
-        self.domain_classifier = None  # TODO
-        self.category_classifier = None  # TODO
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU(),
+
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU()
+        )
+        self.category_encoder = nn.Sequential(
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU(),
+
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU(),
+
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
+            nn.ReLU()
+        )
+
+        self.domain_classifier = nn.Linear( 512, 2 )
+        self.object_classifier = nn.Linear( 512, 7 )
 
         self.reconstructor = None  # TODO
         raise NotImplementedError( '[TODO] Implement DomainDisentangleModel' )
 
-    def forward(self, x):
-        # TODO
-        raise NotImplementedError( '[TODO] Implement DomainDisentangleModel forward() method' )
+    def forward(self, x, w1=None, w2=None, w3=None, w4=None, w5=None):
+        x = self.feature_extractor( x )
+        if w1 is not None:
+            x = self.category_encoder( x )
+            x = self.object_classifier( x )
+        elif w2 is not None:
+            x = self.domain_encoder( x )
+            x = self.domain_classifier( x )
+        elif w3 is not None:
+            x = self.category_encoder( x )
+            x = self.domain_classifier( x )
+        elif w4 is not None:
+            x = self.domain_encoder( x )
+            x = self.object_classifier( x )
+        else:
+            y = self.category_encoder( x )
+            z = self.domain_encoder( x )
+            y.extend( z )
+            x = self.reconstructor( y )
+        return x
