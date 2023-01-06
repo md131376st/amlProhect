0847204ff0f5728d0a4f47b498b3e7bae7d46792
MONA DAVARI s296589 <s296589@studenti.polito.it>
2023-01-06 15:08:39 +0100

    fix small mistake

--- main.py
+++ main.py
@@ -65,26 +65,26 @@ def main(opt):
                 if iteration > opt['max_iterations']:
                     break
 
-            for data in test_loader:
-
-                total_train_loss += experiment.train_iteration( data, train=False )
-
-                if iteration % opt['print_every'] == 0:
-                    logging.info( f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}' )
-
-                if iteration % opt['validate_every'] == 0:
-                    # Run validation
-                    val_accuracy, val_loss = experiment.validate( validation_loader )
-                    logging.info( f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
-                    if val_accuracy > best_accuracy:
-                        experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
-                                                    best_accuracy, total_train_loss )
-                    experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration, best_accuracy,
-                                                total_train_loss )
-
-                iteration += 1
-                if iteration > opt['max_iterations']:
-                    break
+            # for data in test_loader:
+            #
+            #     total_train_loss += experiment.train_iteration( data, train=False )
+            #
+            #     if iteration % opt['print_every'] == 0:
+            #         logging.info( f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}' )
+            #
+            #     if iteration % opt['validate_every'] == 0:
+            #         # Run validation
+            #         val_accuracy, val_loss = experiment.validate( validation_loader )
+            #         logging.info( f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
+            #         if val_accuracy > best_accuracy:
+            #             experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
+            #                                         best_accuracy, total_train_loss )
+            #         experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration, best_accuracy,
+            #                                     total_train_loss )
+            #
+            #     iteration += 1
+            #     if iteration > opt['max_iterations']:
+            #         break
 
     # Test
     experiment.load_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth' )
