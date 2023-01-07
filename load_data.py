f4c1397f4f473a92cf354627242497a7cef6a6dc
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-07 16:04:00 +0100

    load_data modified

--- load_data.py
+++ load_data.py
@@ -12,6 +12,13 @@ CATEGORIES = {
     'person': 6,
 }
 
+DOMAINS = {
+    'art_painting': 0,
+    'cartoon': 1,
+    'photo': 1,
+    'sketch': 1
+}
+
 class PACSDatasetBaseline(Dataset):
     def __init__(self, examples, transform):
         self.examples = examples
@@ -33,13 +40,36 @@ def read_lines(data_path, domain_name):
     for line in lines: 
         line = line.strip().split()[0].split('/')
         category_name = line[3]
-        category_idx = CATEGORIES[category_name]
+        domain_name = line[2]
+        category_idx = DOMAINS[category_name]
+        category_idx = DOMAINS[category_name]
         image_name = line[4]
         image_path = f'{data_path}/kfold/{domain_name}/{category_name}/{image_name}'
         if category_idx not in examples.keys():
             examples[category_idx] = [image_path]
         else:
             examples[category_idx].append(image_path)
+    return examples
+
+def read_lines_domain_disentangle(data_path, domain_name):
+    examples = {}
+    with open(f'{data_path}/{domain_name}.txt') as f:
+        lines = f.readlines()
+
+    for line in lines: 
+        line = line.strip().split()[0].split('/')
+        category_name = line[3]
+        domain_name = line[2]
+        category_idx = DOMAINS[category_name]
+        domain_idx = DOMAINS[category_name]
+        image_name = line[4]
+        image_path = f'{data_path}/kfold/{domain_name}/{category_name}/{image_name}'
+        if domain_idx not in examples.keys():
+            examples[domain_idx][category_idx] = [image_path]
+        elif category_idx not in examples[domain_idx].keys():
+            examples[domain_idx][category_idx] = [image_path]
+        else:
+            examples[domain_idx][category_idx].append(image_path)
     return examples
 
 def build_splits_baseline(opt):
@@ -99,7 +129,60 @@ def build_splits_baseline(opt):
     return train_loader, val_loader, test_loader
 
 def build_splits_domain_disentangle(opt):
-    raise NotImplementedError('[TODO] Implement build_splits_domain_disentangle') #TODO
+    source_domain = 'art_painting'
+    target_domain = opt['target_domain']
+
+    source_examples = read_lines(opt['data_path'], source_domain)
+    target_examples = read_lines(opt['data_path'], target_domain)
+
+    # Compute ratios of examples for each category
+    source_category_ratios = {category_idx: len(examples_list) for category_idx, examples_list in source_examples.items()}
+    source_total_examples = sum(source_category_ratios.values())
+    source_category_ratios = {category_idx: c / source_total_examples for category_idx, c in source_category_ratios.items()}
+
+    # Build splits - we train only on the source domain (Art Painting)
+    val_split_length = source_total_examples * 0.2 # 20% of the training split used for validation
+
+    train_examples = []
+    val_examples = []
+    test_examples = []
+
+    for domain_idx, category_idx, examples_list in source_examples.items():
+        split_idx = round(source_category_ratios[category_idx] * val_split_length)
+        for i, example in enumerate(examples_list):
+            if i > split_idx:
+                train_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+            else:
+                val_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+    
+    for domain_idx, category_idx, examples_list in target_examples.items():
+        for example in examples_list:
+            test_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+    
+    # Transforms
+    normalize = T.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # ResNet18 - ImageNet Normalization
+
+    train_transform = T.Compose([
+        T.Resize(256),
+        T.RandAugment(3, 15),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        normalize
+    ])
+
+    eval_transform = T.Compose([
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        normalize
+    ])
+
+    # Dataloaders
+    train_loader = DataLoader(PACSDatasetBaseline(train_examples, train_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
+    val_loader = DataLoader(PACSDatasetBaseline(val_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+    test_loader = DataLoader(PACSDatasetBaseline(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+
+    return train_loader, val_loader, test_loader
 
 def build_splits_clip_disentangle(opt):
     raise NotImplementedError('[TODO] Implement build_splits_clip_disentangle') #TODO
