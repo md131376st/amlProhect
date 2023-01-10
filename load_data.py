3028f3e942dd330f62d57bd4d4a23711a91d205c
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-10 10:59:08 +0100

    getitem modified

--- load_data.py
+++ load_data.py
@@ -26,9 +26,9 @@ class PACSDatasetBaseline(Dataset):
         return len(self.examples)
     
     def __getitem__(self, index):
-        img_path, y = self.examples[index]
+        img_path, y, z = self.examples[index]
         x = self.transform(Image.open(img_path).convert('RGB'))
-        return x, y
+        return x, y, z
 
 def read_lines(data_path, domain_name):
     examples = {}
