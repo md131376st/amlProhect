4e6a5d1fd644dd67fa2fe4c6d9ad3a750cd00151
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-14 18:44:25 +0100

    train iteration modified

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -74,17 +74,17 @@ class DomainDisentangleExperiment: # See point 2. of the project
             loss = self.domain_classifier_criterion(logits, z)
             loss.backward()
 
-            # logits = self.model(x, w3=1)
-            # loss = self.domain_category_criterion(logits)
-            # loss.backward()
+            logits = self.model(x, w3=1)
+            loss = self.domain_category_criterion(logits)
+            loss.backward()
             
             logits = self.model(x, w4=1)
             loss = self.object_domain_criterion(logits)
             loss.backward()
 
-            # logits, X = self.model(x, w5=1)
-            # loss = self.reconstructor_criterion(logits, X)
-            # loss.backward()
+            logits, X = self.model(x, w5=1)
+            loss = self.reconstructor_criterion(logits, X)
+            loss.backward()
 
         else:
             x, y, z = data
@@ -95,13 +95,13 @@ class DomainDisentangleExperiment: # See point 2. of the project
             loss = self.domain_classifier_criterion(logits, z)
             loss.backward()
 
-            # logits = self.model(x, w3=1)
-            # loss = self.domain_category_criterion(logits)
-            # loss.backward()
+            logits = self.model(x, w3=1)
+            loss = self.domain_category_criterion(logits)
+            loss.backward()
 
-            # logits, X = self.model(x, w5=1)
-            # loss = self.reconstructor_criterion(logits, X)
-            # loss.backward()
+            logits, X = self.model(x, w5=1)
+            loss = self.reconstructor_criterion(logits, X)
+            loss.backward()
 
         self.optimizer.step()
 
