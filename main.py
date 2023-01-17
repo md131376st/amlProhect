344b11e9e7c104c83859ac0b895b3de69fcbfb6d
MONA DAVARI s296589 <s296589@studenti.polito.it>
2023-01-17 17:42:27 +0100

    not sure changes

--- main.py
+++ main.py
@@ -46,58 +46,57 @@ def main(opt):
             logging.info( opt )
 
         # Train loop
+        for i in range( 1000 ):
+            weight = torch.rand( 5 ) / 1000
+            print( weight )
+            while iteration < opt['max_iterations']:
+                for data in train_loader:
+                    if opt['experiment'] == 'domain_disentangle':
+                        total_train_loss += experiment.train_iteration( data, train=True, weight=weight )
 
+                    if iteration % opt['print_every'] == 0:
+                        logging.info(
+                            f'[TRAIN - {iteration} - Weight- {weight}] Loss: {total_train_loss / (iteration + 1)}' )
 
+                    if iteration % opt['validate_every'] == 0:
+                        # Run validation
+                        val_accuracy, val_loss = experiment.validate( validation_loader )
+                        logging.info(
+                            f'[VAL - {iteration} Weight- {weight}]] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
+                        if val_accuracy > best_accuracy:
+                            experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
+                                                        best_accuracy, total_train_loss )
+                        experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration,
+                                                    best_accuracy,
+                                                    total_train_loss )
 
-        while iteration < opt['max_iterations']*1000:
-            for data in train_loader:
-                if opt['experiment'] == 'domain_disentangle':
-                    for i in range( 1000 ):
-                        weight = torch.rand(5)/1000
-                        print(weight)
-                        total_train_loss += experiment.train_iteration( data, True, weight )
+                    iteration += 1
+                    if iteration > opt['max_iterations']:
+                        break
+
+                if iteration <= opt['max_iterations']:
+                    for data in test_loader:
+
+                        total_train_loss += experiment.train_iteration( data, train=False, weight=weight )
 
                         if iteration % opt['print_every'] == 0:
                             logging.info( f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}' )
-                            logging.info( f'[lostFnctionWeight - {weight}]' )
 
                         if iteration % opt['validate_every'] == 0:
                             # Run validation
                             val_accuracy, val_loss = experiment.validate( validation_loader )
-                            logging.info( f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
-                            logging.info( f'[lostFnctionWeight - {weight}]')
+                            logging.info(
+                                f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
                             if val_accuracy > best_accuracy:
                                 experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
                                                             best_accuracy, total_train_loss )
-                            experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration, best_accuracy,
+                            experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration,
+                                                        best_accuracy,
                                                         total_train_loss )
 
                         iteration += 1
                         if iteration > opt['max_iterations']:
                             break
-
-                        if iteration <= opt['max_iterations']*1000:
-                            for data in test_loader:
-
-                                total_train_loss += experiment.train_iteration( data, train=False, weight=weight )
-
-                                if iteration % opt['print_every'] == 0:
-                                    logging.info( f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}' )
-
-                                if iteration % opt['validate_every'] == 0:
-                                    # Run validation
-                                    val_accuracy, val_loss = experiment.validate( validation_loader )
-                                    logging.info( f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
-                                    if val_accuracy > best_accuracy:
-                                        experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
-                                                                    best_accuracy, total_train_loss )
-                                    experiment.save_checkpoint( f'{opt["output_path"]}/last_checkpoint.pth', iteration,
-                                                                best_accuracy,
-                                                                total_train_loss )
-
-                                iteration += 1
-                                if iteration > opt['max_iterations']:
-                                    break
 
     # Test
     experiment.load_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth' )
