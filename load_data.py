9c138c2f4687ae354e9e9abb82c9be0d9f6bcc13
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-20 13:01:52 +0100

    load data modified

--- load_data.py
+++ load_data.py
@@ -26,6 +26,19 @@ class PACSDatasetBaseline(Dataset):
         return len(self.examples)
     
     def __getitem__(self, index):
+        img_path, y= self.examples[index]
+        x = self.transform(Image.open(img_path).convert('RGB'))
+        return x, y
+
+class PACSDatasetDomainDisentangle(Dataset):
+    def __init__(self, examples, transform):
+        self.examples = examples
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.examples)
+    
+    def __getitem__(self, index):
         img_path, y, z = self.examples[index]
         x = self.transform(Image.open(img_path).convert('RGB'))
         return x, y, z
@@ -48,6 +61,26 @@ def read_lines(data_path, domain_name):
     return examples
 
 def read_lines_domain_disentangle(data_path, domain_name):
+    examples = {}
+    with open(f'{data_path}/{domain_name}.txt') as f:
+        lines = f.readlines()
+
+    for line in lines: 
+        line = line.strip().split()[0].split('/')
+        category_name = line[3]
+        #domain_name = line[2]
+        category_idx = CATEGORIES[category_name]
+        domain_idx = DOMAINS[domain_name]
+        domain_category = (domain_idx, category_idx)
+        image_name = line[4]
+        image_path = f'{data_path}/kfold/{domain_name}/{category_name}/{image_name}'
+        if domain_category not in examples.keys():
+            examples[domain_category] = [image_path]
+        else:
+            examples[domain_category].append(image_path)
+    return examples
+
+def read_lines_clip_disentangle(data_path, domain_name):
     examples = {}
     with open(f'{data_path}/{domain_name}.txt') as f:
         lines = f.readlines()
@@ -183,11 +216,74 @@ def build_splits_domain_disentangle(opt):
     ])
 
     # Dataloaders
-    train_loader = DataLoader(PACSDatasetBaseline(train_examples, train_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
-    val_loader = DataLoader(PACSDatasetBaseline(val_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
-    test_loader = DataLoader(PACSDatasetBaseline(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+    train_loader = DataLoader(PACSDatasetDomainDisentangle(train_examples, train_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
+    val_loader = DataLoader(PACSDatasetDomainDisentangle(val_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+    test_loader = DataLoader(PACSDatasetDomainDisentangle(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
 
     return train_loader, val_loader, test_loader
 
 def build_splits_clip_disentangle(opt):
-    raise NotImplementedError('[TODO] Implement build_splits_clip_disentangle') #TODO
+    source_domain = 'art_painting'
+    target_domain = opt['target_domain']
+
+    source_examples = read_lines_clip_disentangle(opt['data_path'], source_domain)
+    target_examples = read_lines_clip_disentangle(opt['data_path'], target_domain)
+
+    # Compute ratios of examples for each category
+    source_category_ratios = dict()
+    for domain_category, examples_list in source_examples.items():
+        if domain_category[1] not in source_category_ratios.keys():
+            source_category_ratios[domain_category[1]] = len(examples_list)
+        else:
+            source_category_ratios[domain_category[1]] += len(examples_list)
+    #source_category_ratios = {domain_category[1]: len(examples_list) for domain_category, examples_list in source_examples.items()}
+    source_total_examples = sum(source_category_ratios.values())
+    source_category_ratios = {category_idx: c / source_total_examples for category_idx, c in source_category_ratios.items()}
+
+    # Build splits - we train only on the source domain (Art Painting)
+    val_split_length = source_total_examples * 0.2 # 20% of the training split used for validation
+
+    train_examples = []
+    val_examples = []
+    test_examples = []
+
+    for domain_category, examples_list in source_examples.items():
+        domain_idx = domain_category[0]
+        category_idx = domain_category[1]
+        split_idx = round(source_category_ratios[category_idx] * val_split_length)
+        for i, example in enumerate(examples_list):
+            if i > split_idx:
+                train_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+            else:
+                val_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+    
+    for domain_category, examples_list in target_examples.items():
+        domain_idx = domain_category[0]
+        category_idx = domain_category[1]
+        for example in examples_list:
+            test_examples.append([example, category_idx, domain_idx]) # each pair is [path_to_img, class_label, domain_label]
+    
+    # Transforms
+    normalize = T.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # ResNet18 - ImageNet Normalization
+
+    train_transform = T.Compose([
+        T.Resize(256),
+        T.RandAugment(3, 15),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        normalize
+    ])
+
+    eval_transform = T.Compose([
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        normalize
+    ])
+
+    # Dataloaders
+    train_loader = DataLoader(PACSDatasetDomainDisentangle(train_examples, train_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
+    val_loader = DataLoader(PACSDatasetDomainDisentangle(val_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+    test_loader = DataLoader(PACSDatasetDomainDisentangle(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+
+    return train_loader, val_loader, test_loader
