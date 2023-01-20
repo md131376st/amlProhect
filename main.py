f8f4a68895f3b45dcd97a8b39351765a5bab37e0
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-20 13:07:03 +0100

    main modified

--- main.py
+++ main.py
@@ -54,13 +54,13 @@ def main(opt):
 
                     if iteration % opt['print_every'] == 0:
                         logging.info(
-                            f'[TRAIN - {iteration} - Weight- {weight}] Loss: {total_train_loss / (iteration + 1)}' )
+                            f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}' )
 
                     if iteration % opt['validate_every'] == 0:
                         # Run validation
                         val_accuracy, val_loss = experiment.validate( validation_loader )
                         logging.info(
-                            f'[VAL - {iteration} Weight- {weight}]] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
+                            f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}' )
                         if val_accuracy > best_accuracy:
                             experiment.save_checkpoint( f'{opt["output_path"]}/best_checkpoint.pth', iteration,
                                                         best_accuracy, total_train_loss )
@@ -81,6 +81,7 @@ def main(opt):
                 logging.info( opt )
 
             # Train loop
+
             iteration = 0
             best_accuracy = 0
             total_train_loss = 0
