297f58138cd2f9528369a098f80f3a689d6eced8
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-20 11:48:22 +0100

    weights modified

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main(opt):
             iteration = 0
             best_accuracy = 0
             total_train_loss = 0
-            weight = torch.tensor( [7.5448e-04, 8.6793e-04, 5.4548e-06] )
+            weight = torch.tensor( [7.5448e-05, 8.6793e-05, 5.4548e-07] )
             print( weight )
             while iteration < opt['max_iterations']:
                 for data in train_loader:
