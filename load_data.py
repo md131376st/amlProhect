e3dec8323f7945f1af316272dbd19c58f5d190a3
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-24 17:52:39 +0100

    weights modified

--- load_data.py
+++ load_data.py
@@ -284,6 +284,6 @@ def build_splits_clip_disentangle(opt):
     # Dataloaders
     train_loader = DataLoader(PACSDatasetDomainDisentangle(train_examples, train_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
     val_loader = DataLoader(PACSDatasetDomainDisentangle(val_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
-    test_loader = DataLoader(PACSDatasetDomainDisentangle(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=False)
+    test_loader = DataLoader(PACSDatasetDomainDisentangle(test_examples, eval_transform), batch_size=opt['batch_size'], num_workers=opt['num_workers'], shuffle=True)
 
     return train_loader, val_loader, test_loader
