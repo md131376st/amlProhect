07eaaf7b8035b150cca5db1be4e71f01dd12948c
Nazanin M <nazanin@Nazanins-Air.wind3.hub>
2023-01-26 11:43:50 +0100

    reconstructor modified

--- models/base_model.py
+++ models/base_model.py
@@ -97,6 +97,10 @@ class DomainDisentangleModel( nn.Module ):
 
             nn.Linear( 512, 512 ),
             nn.BatchNorm1d( 512 ),
+            nn.ReLU(),
+
+            nn.Linear( 512, 512 ),
+            nn.BatchNorm1d( 512 ),
             nn.ReLU()
         ) 
 
